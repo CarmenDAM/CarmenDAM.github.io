ec01b507ac02edf1f006be2bb95a52ece75cf310
CarmenDAM <96106243+CarmenDAM@users.noreply.github.com>
2022-06-06 16:39:46 +0200

    Update Boton.test.jsx

--- Boton.test.jsx
+++ Boton.test.jsx
@@ -5,20 +5,20 @@ import Boton from './Boton';
 
 test('Tests unitarios boton', ()  => {
     // Definimos una función para pasar al botón
-    //const funcionTest = () => {console.log("hola :)")};
+    //const funcionTest = () => {console.log("Test del botón")};
     // O mockeamos la función
     const funcionTest = jest.fn();
-    const botonTest = render(<Boton text="Holi :D" onClickButton={funcionTest} />);
+    const botonTest = render(<Boton text="Test del botón" onClickButton={funcionTest} />);
 
     // Mockeamos la consola 
     //console.log = jest.fn();
     // Testeando elemento
-    const boton = botonTest.getByText("Holi :D");
+    const boton = botonTest.getByText("Test del botón");
     console.log(prettyDOM(boton));
     //.click();
     fireEvent.click(boton);
     expect(funcionTest).toHaveBeenCalledTimes(1);
     // Testeando llamada a la función del botón (imprime en consola)
-    //expect(console.log.mock.calls[0][0]).toBe('hola :)');
+    //expect(console.log.mock.calls[0][0]).toBe('Test del botón');
 
 });
